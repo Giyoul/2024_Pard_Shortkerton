7da0fbc74ab29a314da4eb05e0ebcdd9cd029690
Giyoul <cla12093@gmail.com>
2024-05-24 21:11:52 +0900

    :seedling: Add Frame Code

---
+++ GiyoungPyoRyuGi/src/main/java/com/example/giyoungpyoryugi/GiyoungPyoRyuGiApplication.java
@@ -0,0 +1,13 @@
+package com.example.giyoungpyoryugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiyoungPyoRyuGiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiyoungPyoRyuGiApplication.class, args);
+    }
+
+}
---
+++ GiyoungPyoRyuGi/src/test/java/com/example/giyoungpyoryugi/GiyoungPyoRyuGiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giyoungpyoryugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiyoungPyoRyuGiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
